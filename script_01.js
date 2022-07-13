a2b22b37a72fd1284c3e5e335aa073edb4fd4f39
Cing73 <craig@vancleaf>
2022-07-13 11:06:51 +0200

    new

--- script_01.js
+++ script_01.js
@@ -3,19 +3,19 @@
 /* Die Satzbau-Maschine | Arrays */
 
 /*** 02. Theorie: Array */
-let arr;
-// arr = new Array();  // Konstruktor
-// arr = []; // Literal
-// arr = [2,7,11,4]; // 4 Elemente
-// arr = [false,true,true];
-// arr = ["Ich","bin","Maxine","Mützerich"];
-arr = [2,7,11,4,5,6,7,8,9,"I am 10"]; // 4 Elemente
+// let arr;
+// // arr = new Array();  // Konstruktor
+// // arr = []; // Literal
+// // arr = [2,7,11,4]; // 4 Elemente
+// // arr = [false,true,true];
+// // arr = ["Ich","bin","Maxine","Mützerich"];
+// arr = [2,7,11,4,5,6,7,8,9,"I am 10"]; // 4 Elemente
 
-output(arr);
-output(arr.length);  // Anzahl der Elemente
-output(arr[0]);  // Index 0 (1. Pos. im Array)
-output(arr[3]);  // Index 3 (letzte Pos. im Array)
-output(arr[arr.length-1]);  // (letzte Pos. im Array)
+// output(arr);
+// output(arr.length);  // Anzahl der Elemente
+// output(arr[0]);  // Index 0 (1. Pos. im Array)
+// output(arr[3]);  // Index 3 (letzte Pos. im Array)
+// output(arr[arr.length-1]);  // (letzte Pos. im Array)
 
 
 
